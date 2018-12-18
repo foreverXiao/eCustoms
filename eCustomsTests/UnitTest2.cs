4a7d71709918f0f441cb0d04bff82dbf54dd7893
foreverXiao <forever_xiao@aliyun.com>
2018-12-18 08:28:52 +0800

    Add files via upload

---
+++ eCustomsTests/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eCustomsTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("abc", "cba");
+        }
+    }
+}
